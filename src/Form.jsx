5ef4635beba02d7429918464de17a02cc98ace25
yummygoods <melissa@yummygoods.com>
2023-07-16 18:34:36 -0400

    adding astro data

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,26 +1,41 @@
 import './form.css';
 import { useState } from 'react';
-import Result from './result.jsx';
+import Result from './Result.jsx';
 
 export default function Form() {
 	const [city, setCity] = useState('');
 	// const [shouldShowResult, setShouldShowResult] = useState(false);
 	const [weather, setWeather] = useState({ data: '' });
 
+	const [astroData, setAstroData] = useState({ data: '' });
+
 	const handleChange = (e) => {
 		setCity(e.target.value);
 		return city;
 	};
 
-	let url = `https://api.weatherapi.com/v1/current.json?key=d01e96a14a9449a7a6f152442231805&q=${city}&aqi=no`;
+	let currentUrl = `https://api.weatherapi.com/v1/current.json?key=d01e96a14a9449a7a6f152442231805&q=${city}&aqi=no`;
+
+	let astroUrl = `https://api.weatherapi.com/v1/astronomy.json?key=d01e96a14a9449a7a6f152442231805&q=${city}&aqi=no`;
+
+
+
+async function fetchAstro() {
+	const response = await fetch(astroUrl);
+	const currentAstro = await response.json();
+	console.log(currentAstro);
+	setAstroData(currentAstro);
+	return astroData;
+}
 
 	async function fetchWeather() {
 		console.log('inside fetch function');
-		const response = await fetch(url);
+		const response = await fetch(currentUrl);
 		const currentWeather = await response.json();
 		console.log(currentWeather);
 		setWeather(currentWeather);
 		return weather;
+		fetchAstro();
 	}
 
 	const handleClick = (e) => {
@@ -32,7 +47,10 @@ export default function Form() {
 	};
 
 	// console.log(weather.location?.name);
-	console.log(weather);
+	console.log(weather, astroData);
+
+
+
 	return (
 		<form id="cityForm">
 			<label htmlFor="city">enter your city or zip code</label>
@@ -46,14 +64,18 @@ export default function Form() {
 			<button type="submit" onClick={handleClick}>
 				submit
 			</button>
-			<Result locationName={weather.location?.name}  currentWeather={weather.current}/>
-			{/* <Result locationName={weather.location.name}  currentWeather={weather.current}/> */}
-			{/* <Result weather={weather.currentWeather && {weather}}/> */}
-			{/* {shouldShowResult && <Result 
-			location={weather.location} 
-			current={weather.current} 
-			
-			/>} */}
+			<Result 
+			locationName={weather.location?.name}  currentWeather={weather.current}
+			astroData={astroData.astronomy.astro}/>
+		
 		</form>
 	);
 }
+
+	// {/* <Result locationName={weather.location.name}  currentWeather={weather.current}/> */}
+	// 		{/* <Result weather={weather.currentWeather && {weather}}/> */}
+	// 		{/* {shouldShowResult && <Result 
+	// 		location={weather.location} 
+	// 		current={weather.current} 
+			
+	// 		/>} */}
--- src/Result.jsx
+++ src/Result.jsx
@@ -10,15 +10,17 @@ return(
 <div id="weather">
     <h3>{props.locationName ? props.locationName : ''}</h3>
     <h3>{props.currentWeather?.temp_f ? props.currentWeather.temp_f : ''}</h3>
+    <h3>{props.currentWeather?.condition.text ? props.currentWeather.condition.text : ''}</h3>
   </div>
 <div id="astroDiv">
-  {/* <!-- populates with data -->
 
-  <!-- populates with data -->
 
-  <!-- populates with data -->
+<h3>{props.astroData?.sunrise ? props.astroData : ''}</h3>
+{astroData.sunrise}
+{astroData.sunset}
 
-  <!-- populates with data --> */}
+
+
 </div>
 </section>
 
